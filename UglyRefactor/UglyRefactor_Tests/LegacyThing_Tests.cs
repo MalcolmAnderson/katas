32e12f2a803d80fa460dc85fc3666b91904e3a1d
Malcolm Anderson <Malcolm Anderson>
2016-07-11 14:15:52 -0700

    Basic Setup - Test file exists, but no integration tests

---
+++ UglyRefactor/UglyRefactor_Tests/LegacyThing_Tests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UglyRefactor;
+
+namespace UglyRefactor
+{
+    [TestFixture]
+    public class LegacyThing_Tests
+    {
+        [Test]
+        public void TestMethod()
+        {
+            LegacyThing o = new LegacyThing();
+        }
+    }
+}
